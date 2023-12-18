7fbcc8977eaf407f44c76084df6993e5da3b473a
Farhan Aziz <farhan@aththariq.com>
2023-12-18 22:07:50 +0700

    Add shadcn

---
+++ lib/utils.ts
@@ -0,0 +1,33 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { z } from 'zod';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function getErrorMessage(error: unknown) {
+  let message: string;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === 'object' && 'message' in error) {
+    message = String(error.message);
+  } else if (typeof error === 'string') {
+    message = error;
+  } else {
+    message = 'Something went wrong, please try again later.';
+  }
+
+  return message;
+}
+
+export function getZodErrorMessage(result: z.SafeParseError<any>) {
+  let errorMessage = '';
+
+  result.error.issues.forEach((issue) => {
+    errorMessage = errorMessage + issue.path[0] + ': ' + issue.message + '. ';
+  });
+
+  return errorMessage;
+}
