c38d5b6e3843f69823ca8153b84bb04ef9e4199c
Farhan Aziz <farhan@aththariq.com>
2023-12-19 17:08:44 +0700

    Update heading tags in sign-in and sign-up pages

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -9,9 +9,9 @@ const SignInPage = ({
 
   return (
     <main className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 ">
-      <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight ">
+      <h1 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight ">
         Sign in to your account
-      </h2>
+      </h1>
 
       <section className="mt-8 sm:mx-auto sm:w-full sm:max-w-[480px]">
         <div className="border bg-card px-6 py-12 shadow sm:rounded-lg sm:px-12 mx-6">
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,21 +1,15 @@
 import SignUpForm from '@/components/forms/SignUpForm';
 
-const SignUpPage = ({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) => {
-  const callbackUrl = searchParams.callbackUrl;
-
+const SignUpPage = () => {
   return (
     <main className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 ">
-      <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight ">
+      <h1 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight ">
         Create an account
-      </h2>
+      </h1>
 
       <section className="mt-8 sm:mx-auto sm:w-full sm:max-w-[480px]">
         <div className="border bg-card px-6 py-12 shadow sm:rounded-lg sm:px-12 mx-6">
-          <SignUpForm callbackUrl={callbackUrl} />
+          <SignUpForm />
         </div>
       </section>
     </main>
