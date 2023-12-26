c9826c170390892fb64b45bf9009f0f8c1356428
Farhan Aziz <farhan@aththariq.com>
2023-12-26 20:08:20 +0700

    Add userId check in GET request handler

--- app/api/auth/verify-email/route.ts
+++ app/api/auth/verify-email/route.ts
@@ -6,15 +6,11 @@ import { getBackendApi } from '@/lib/axios';
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const token = searchParams.get('token') as string;
+  const userId = searchParams.get('id') as string;
   if (!token) NextResponse.json({ error: 'Token not found' }, { status: 500 });
+  if (!userId) NextResponse.json({ error: 'ID not found' }, { status: 500 });
 
   try {
-    const decoded = jwt.verify(token, process.env.EMAIL_SECRET!);
-    if (typeof decoded === 'string') {
-      throw new Error('Invalid token, please request a new one.');
-    }
-    const userId = decoded.user.id;
-
     const data = { emailVerified: true };
     await getBackendApi(token).patch(`/users/${userId}/emailVerified`, data);
 
