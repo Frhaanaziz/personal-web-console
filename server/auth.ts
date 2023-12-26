b847c347694831b04260dd8c396b1636aaf6a3ad
Farhan Aziz <farhan@aththariq.com>
2023-12-26 14:37:41 +0700

    Update authOptions in auth.ts

--- server/auth.ts
+++ server/auth.ts
@@ -71,23 +71,21 @@ export const authOptions = {
     async signIn({ user, account, profile }) {
       if (profile && account && account.provider === 'google') {
         const input = {
-          data: {
-            name: profile.name,
-            email: profile.email,
-            emailVerified: profile.email_verified,
-            image: profile.picture,
-            accounts: {
-              create: {
-                type: account.type,
-                provider: account.provider,
-                providerAccountId: account.providerAccountId,
-                access_token: account.access_token,
-                expires_at: account.expires_at,
-                token_type: account.token_type,
-                scope: account.scope,
-                id_token: account.id_token,
-              },
-            },
+          name: profile.name,
+          email: profile.email,
+          emailVerified: profile.email_verified,
+          image: profile.picture,
+          accounts: {
+            type: account.type,
+            provider: account.provider,
+            providerAccountId: account.providerAccountId,
+            refresh_token: account.refresh_token,
+            access_token: account.access_token,
+            expires_at: account.expires_at,
+            token_type: account.token_type,
+            scope: account.scope,
+            id_token: account.id_token,
+            session_state: account.session_state,
           },
         };
 
