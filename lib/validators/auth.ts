8a77af8d25259d8d405525c4ecfdde2da697b7fe
Farhan Aziz <farhan@aththariq.com>
2023-12-26 20:14:40 +0700

    Add Google login schema to auth validators

--- lib/validators/auth.ts
+++ lib/validators/auth.ts
@@ -37,3 +37,22 @@ export const resetPasswordSchema = z
     message: "Password don't match",
     path: ['confirmPassword'],
   });
+
+export const googleLoginSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  emailVerified: z.boolean(),
+  image: z.string().nullable().optional(),
+  accounts: z.object({
+    type: z.string(),
+    provider: z.string(),
+    providerAccountId: z.string(),
+    refresh_token: z.string().nullable().optional(),
+    access_token: z.string().nullable().optional(),
+    expires_at: z.number().nullable().optional(),
+    token_type: z.string().nullable().optional(),
+    scope: z.string().nullable().optional(),
+    id_token: z.string().nullable().optional(),
+    session_state: z.string().nullable().optional(),
+  }),
+});
