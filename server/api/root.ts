ec6562821428066897951199ce27db2c2705d688
Farhan Aziz <farhan@aththariq.com>
2023-12-26 14:37:29 +0700

    Remove auth router and its dependencies

--- server/api/root.ts
+++ server/api/root.ts
@@ -1,10 +1,8 @@
 import { router } from '@/server/api/trpc';
-import { auth } from '@/server/api/routers/auth';
 import { keyword } from './routers/keyword';
 import { content } from './routers/content';
 
 export const appRouter = router({
-  auth,
   keyword,
   content,
 });
