73300257f8fb7f8314831a46e96728d4e7b2613b
Farhan Aziz <farhan@aththariq.com>
2023-12-29 09:26:37 +0700

    Refactor keyword validation schemas

--- lib/validators/keyword.ts
+++ lib/validators/keyword.ts
@@ -1,13 +1,23 @@
 import { z } from 'zod';
 
 export const newKeywordSchema = z.object({
-  keyword: z.string().min(1),
-  group: z.string().min(1),
-  content: z.string().min(1),
+  keyword: z
+    .string()
+    .min(1, { message: 'Keyword must be at least 1 character long.' }),
+  group: z
+    .string()
+    .min(1, { message: 'Group must be at least 1 character long.' }),
+  content: z
+    .string()
+    .min(1, { message: 'Content must be at least 1 character long.' }),
 });
 
 export const updateKeywordSchema = z.object({
-  id: z.string().cuid(),
-  keyword: z.string().min(1),
-  group: z.string().min(1),
+  id: z.string().cuid({ message: 'Invalid keyword id.' }),
+  keyword: z
+    .string()
+    .min(1, { message: 'Keyword must be at least 1 character long.' }),
+  group: z
+    .string()
+    .min(1, { message: 'Group must be at least 1 character long.' }),
 });
