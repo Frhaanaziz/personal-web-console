35f2128fca717886de355927bc9f31c48987ac19
Farhan Aziz <farhan@aththariq.com>
2023-12-29 16:12:16 +0700

    Add BreadCrumbs component to Heading components

--- components/HeadingNoAction.tsx
+++ components/HeadingNoAction.tsx
@@ -1,8 +1,13 @@
+import BreadCrumbs from './BreadCrumbs';
+
 const HeadingNoAction = ({ text }: { text: string }) => {
   return (
-    <div className="pb-5 sm:flex sm:items-center sm:justify-between mb-4">
-      <h1 className="text-2xl font-semibold leading-6 ">{text}</h1>
-    </div>
+    <>
+      <BreadCrumbs />
+      <div className="pb-5 sm:flex sm:items-center sm:justify-between mb-4">
+        <h1 className="text-2xl font-semibold leading-6 ">{text}</h1>
+      </div>
+    </>
   );
 };
 
--- components/HeadingWithAction.tsx
+++ components/HeadingWithAction.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { buttonVariants } from './ui/button';
+import BreadCrumbs from './BreadCrumbs';
 
 const HeadingWithAction = ({
   heading,
@@ -11,14 +12,17 @@ const HeadingWithAction = ({
   label: string;
 }) => {
   return (
-    <header className="pb-5 sm:flex sm:items-center sm:justify-between mb-4">
-      <h1 className="text-2xl font-semibold leading-6 ">{heading}</h1>
-      <div className="mt-3 sm:ml-4 sm:mt-0">
-        <Link href={href} className={buttonVariants()}>
-          {label}
-        </Link>
-      </div>
-    </header>
+    <>
+      <BreadCrumbs />
+      <header className="pb-5 sm:flex sm:items-center sm:justify-between mb-4">
+        <h1 className="text-2xl font-semibold leading-6 ">{heading}</h1>
+        <div className="mt-3 sm:ml-4 sm:mt-0">
+          <Link href={href} className={buttonVariants()}>
+            {label}
+          </Link>
+        </div>
+      </header>
+    </>
   );
 };
 
